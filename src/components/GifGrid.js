5abc415c05240daee7e073ba08424b7c4bb555e6
Cristian <cris10x@hotmail.com>
2021-03-12 19:47:20 -0500

    className - clases en css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,45 +1,29 @@
 import React, { useState, useEffect } from 'react'
+import { getGifts } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
     const [images, setImages] = useState([]);
 
-
     useEffect( () => {
-        getGifts();
-    }, []);
-
-    const getGifts = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=mickey&limit=10&api_key=puvheGF2U8EIXwmjP4iZbwsoqUj0d9cH';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages( gifs );
-
-    }
+        getGifts( category )
+            .then( setImages );
+    }, [ category ]);
 
     return (
-        <div>
+        <>
             <h3>{ category }</h3>
-            {
-                images.map( img => (
-                    <GifGridItem
-                        key = { img.id }
-                        { ...img } 
-                    />
-                )) 
-            }
-        </div>
+            <div className="card-grid">
+                {
+                    images.map( img => (
+                        <GifGridItem
+                            key = { img.id }
+                            { ...img } 
+                        />
+                    )) 
+                }
+            </div>
+        </>
     )
 }
